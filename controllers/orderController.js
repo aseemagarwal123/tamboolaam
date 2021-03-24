e8f42852e5891681aefd0458ba90c6195c38fca3
Arzoo Goyal <arzoogoyalchd@gmail.com>
2021-03-24 22:42:25 +0530

    get all orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,6 +3,7 @@ const {Order} = require('../models/order');
 const placedOrder = async (req, res, next)=>{
     try{
         const order = await new Order(req.body);
+        await order.save();
         return res.status(200).send({
             'response': {
                 'message': "order",
@@ -14,20 +15,33 @@ const placedOrder = async (req, res, next)=>{
     }
 }
 
+
+
+
 const getAllOrders = async (req, res, next) => {
     try {
-        const allOrders = await Order.find();
-        console.log(allOrders);
-        allOrders.forEach(order => {
-            res.status(200).send({
+        const page = Number(req.query.limit)*((req.query.page)-1) || 0;
+        const limit = Number(req.query.limit) || 10;
+
+        const allOrders = await Order.find().skip(page).limit(limit);
+        if(allOrders){
+            console.log(allOrders);
+             return res.status(200).send({
                 'response': {
                     'message': "allOrders",
-                    'order': order
+                    'order': allOrders
                 }
             })
-        })
+        }
+        else {
+            return res.status(400).send({
+                'response': {
+                    'message': "no orders"
+                }
+            })
+        }
     } catch(e){
-        next(e);
+        next(e)
     }
 }
 
